9e7ba1d26a1d0d9dfbad40753066dc969ba9b487
vairadurai <vairadurai007@gmail.com>
2022-08-08 16:15:08 +0530

    add

---
+++ pop/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+// pop
+
+let a=["hello","dear","love u"];
+a.pop(); //it remove a last position value in array
+console.log(a);
+
+// output
+
+(2) ['hello', 'dear']
+// 0: "hello"
+// 1: "dear"
