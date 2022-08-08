18750eb884029f88307c5a50a9541505de979e87
vairadurai <vairadurai007@gmail.com>
2022-08-08 16:11:28 +0530

    add

---
+++ indexof/main.js
@@ -0,0 +1,15 @@
+
+
+// INDEX OF
+
+let a=[1,2,3,4,5];
+
+console.log(a[3]);
+let b;
+b=a.indexOf(2); //it gives the index position while we give value present in array
+console.log(b);
+
+// output
+4
+1
+
